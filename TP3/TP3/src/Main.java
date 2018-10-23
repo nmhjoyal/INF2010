0422a4e57e1bbfa4df96bd38e191e3100dd1494a
Nicole Joyal <nicole.joyal@oronospolytechnique.com>
2018-10-23 03:05:00 -1200

    exo2 fini, complexités exo1 (fonctions: contains & getHeight) à revoir

--- TP3/TP3/src/Main.java
+++ TP3/TP3/src/Main.java
@@ -7,8 +7,8 @@ public class Main {
         TestBinary testBinary = new TestBinary();
         testBinary.test();
 //
-//        TestCompany testCompany = new TestCompany();
-//        testCompany.test();
+        TestCompany testCompany = new TestCompany();
+        testCompany.test();
 
         /*NOTRE PROPRE TEST*/
 
@@ -33,6 +33,27 @@ public class Main {
 //        System.out.println(treeTest.getHeight() + "\n");
 //        System.out.println(treeTest.toStringInOrder());
 
-
+//        CompanyTree compTree = new CompanyTree(new CompanyNode(400));
+//        CompanyNode node11 = new CompanyNode(50);
+//        CompanyNode node12 = new CompanyNode(5);
+//        CompanyNode node21 = new CompanyNode(30);
+//        CompanyNode node22 = new CompanyNode(10);
+//        CompanyNode node31 = new CompanyNode(90);
+//        CompanyNode node32 = new CompanyNode(-60);
+//        CompanyNode node33 = new CompanyNode(-30);
+//        CompanyNode node34 = new CompanyNode(-15);
+//
+//        compTree.buy(node11);
+//        compTree.buy(node12);
+//        node11.buy(node21);
+//        node11.buy(node22);
+//        node21.buy(node31);
+//        node21.buy(node32);
+//        node22.buy(node33);
+//        node22.buy(node34);
+//
+//        System.out.println(compTree.getWorstChildMoney() +"\n");
+//        System.out.println(compTree.getMoney() +"\n");
+//        System.out.println(compTree.getTreeInOrder() +"\n");
     }
 }
